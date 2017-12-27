180de4495873ef18b6e2c7260315110c063fc34e
Pawel Panek <panpawel88@gmail.com>
2017-12-27 21:37:50 +0100

    Make AbstractWorkTimeTrackerTest and move it to sharedTest.
    Signed-off-by: Pawel Panek <panpawel88@gmail.com>

--- app/src/test/java/org/ppanek/worktimetracker/model/WorkTimeTrackerDefaultTest.java
+++ app/src/test/java/org/ppanek/worktimetracker/model/WorkTimeTrackerDefaultTest.java
@@ -1,91 +1,12 @@
 package org.ppanek.worktimetracker.model;
 
-import org.junit.Test;
-
-import java.util.Calendar;
-
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertSame;
-import static junit.framework.Assert.fail;
-
 /**
- * Created by pawel on 13.11.2017.
+ * Created by pawel on 27.12.2017.
  */
 
-public class WorkTimeTrackerDefaultTest {
-
-    @Test
-    public void testSetAndGetWorkday() {
-        WorkTimeTrackerDefault tracker = new WorkTimeTrackerDefault();
-        IWorkday aWorkday = tracker.newWorkday();
-
-        Calendar calendar = Calendar.getInstance();
-        tracker.putWorkday(calendar.getTime(), aWorkday);
-
-        assertEquals(aWorkday, tracker.getWorkday(calendar.getTime()));
+public class WorkTimeTrackerDefaultTest extends AbstractWorkTimeTrackerTest {
+    @Override
+    public IWorkTimeTracker createTracker() {
+        return new WorkTimeTrackerDefault();
     }
-
-    @Test
-    public void testInvalidValues() {
-        WorkTimeTrackerDefault tracker = new WorkTimeTrackerDefault();
-        try {
-            tracker.putWorkday(null, null);
-            fail();
-        } catch (IllegalArgumentException e) {
-            // intentionally empty
-        }
-
-        try {
-            tracker.getWorkday(null);
-            fail();
-        } catch (IllegalArgumentException e) {
-            // intentionally empty
-        }
-
-        try {
-            tracker.removeWorkday(null);
-            fail();
-        } catch (IllegalArgumentException e) {
-            // intentionally empty
-        }
-
-        // It's legal to set null workday
-        try {
-            tracker.putWorkday(Calendar.getInstance().getTime(), null);
-        } catch (IllegalArgumentException e) {
-            fail();
-        }
-    }
-
-    @Test
-    public void testSetAndGetWorkdayInSameDay() {
-        WorkTimeTrackerDefault tracker = new WorkTimeTrackerDefault();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 8);
-
-        IWorkday workday = tracker.newWorkday();
-
-        tracker.putWorkday(calendar.getTime(), workday);
-        assertSame(workday, tracker.getWorkday(calendar.getTime()));
-
-        calendar.add(Calendar.HOUR_OF_DAY, 8);
-        assertSame(workday, tracker.getWorkday(calendar.getTime()));
-    }
-
-    @Test
-    public void testSetAndRemoveWorkday() {
-        WorkTimeTrackerDefault tracker = new WorkTimeTrackerDefault();
-        Calendar calendar = Calendar.getInstance();
-        IWorkday workday = tracker.newWorkday();
-
-        tracker.putWorkday(calendar.getTime(), workday);
-
-        assertSame(workday, tracker.getWorkday(calendar.getTime()));
-
-        tracker.removeWorkday(calendar.getTime());
-
-        assertNull(tracker.getWorkday(calendar.getTime()));
-    }
-
 }
