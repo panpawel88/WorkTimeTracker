f09e3f1090af2426a5e712d1bf3fbe63f287bf6e
Pawel Panek <panpawel88@gmail.com>
2018-01-01 21:15:48 +0100

    Remove Nfc related stuff from MainActivity
    Signed-off-by: Pawel Panek <panpawel88@gmail.com>

--- app/src/main/java/org/ppanek/worktimetracker/MainActivity.java
+++ app/src/main/java/org/ppanek/worktimetracker/MainActivity.java
@@ -1,55 +1,36 @@
 package org.ppanek.worktimetracker;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.NfcA;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CalendarFragment.OnFragmentInteractionListener, StatsFragment.OnFragmentInteractionListener {
 
-    public static final String MIME_TEXT_PLAIN = "text/plain";
-
-    private NfcAdapter mNfcAdapter;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home: {
-                    FragmentManager manager = getFragmentManager();
-                    FragmentTransaction transaction = manager.beginTransaction();
-                    transaction.replace(R.id.content, new CalendarFragment());
-                    transaction.commit();
-                    return true;
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home: {
+                        FragmentManager manager = getFragmentManager();
+                        FragmentTransaction transaction = manager.beginTransaction();
+                        transaction.replace(R.id.content, new CalendarFragment());
+                        transaction.commit();
+                        return true;
+                    }
+                    case R.id.navigation_stats: {
+                        FragmentManager manager = getFragmentManager();
+                        FragmentTransaction transaction = manager.beginTransaction();
+                        transaction.replace(R.id.content, new StatsFragment());
+                        transaction.commit();
+                        return true;
+                    }
+                    case R.id.navigation_settings:
+                        return true;
                 }
-                case R.id.navigation_stats: {
-                    FragmentManager manager = getFragmentManager();
-                    FragmentTransaction transaction = manager.beginTransaction();
-                    transaction.replace(R.id.content, new StatsFragment());
-                    transaction.commit();
-                    return true;
-                }
-                case R.id.navigation_settings:
-                    return true;
-            }
-            return false;
-        }
-
-    };
+                return false;
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,84 +39,11 @@ public class MainActivity extends AppCompatActivity implements CalendarFragment.
 
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
-//        transaction.replace(R.id.content, new CalendarFragment());
         transaction.add(R.id.content, new CalendarFragment());
         transaction.commit();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (mNfcAdapter == null) {
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-        handleIntent(getIntent());
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        setupForegroundDispatch(this, mNfcAdapter);
-    }
-
-    @Override
-    protected void onPause() {
-        stopForeqroundDispatch(this, mNfcAdapter);
-        super.onPause();
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    public static String byteArrayToHex(byte[] a) {
-        StringBuilder sb = new StringBuilder(a.length * 2);
-        for(byte b: a)
-            sb.append(String.format("%02x", b));
-        return sb.toString();
-    }
-
-    private void handleIntent(Intent intent) {
-        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-//            String[] techList = tag.getTechList();
-//            String searchedTech = NfcA.class.getName();
-//
-//            for (String tech : techList) {
-//                if (searchedTech.equals(tech)) {
-//                    new NfcaReaderTask().execute(tag);
-//                }
-//            }
-        }
-    }
-
-    private void setupForegroundDispatch(Activity activity, NfcAdapter adapter) {
-        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-        IntentFilter[] filters = new IntentFilter[1];
-        String[][] techList = new String[][]{
-                new String[] {NfcA.class.getName()}
-        };
-
-        filters[0] = new IntentFilter();
-        filters[0].addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
-        try {
-            filters[0].addDataType(MIME_TEXT_PLAIN);
-        } catch (IntentFilter.MalformedMimeTypeException e) {
-            throw new RuntimeException("Check your mime type.");
-        }
-
-        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-    }
-
-    private void stopForeqroundDispatch(Activity activity, NfcAdapter adapter) {
-        adapter.disableForegroundDispatch(activity);
     }
 
     @Override
